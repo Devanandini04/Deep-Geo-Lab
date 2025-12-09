c27d400d100c6e77cc416e9e4842d1ec7a54bf97
Devanandini <devanandini4105@gmail.com>
2025-12-09 23:59:27 +0530

    feat: initialized rasterio track structure

---
+++ rasterio_track/01_ndvi_calc.py
@@ -0,0 +1,10 @@
+# Rasterio Learning Track
+# Goal: Master Satellite Imagery Processing (NDVI, Segmentation)
+
+def setup_environment():
+    print("Environment setup initiated for GeoAI...")
+    # TODO: Install rasterio, matplotlib, numpy
+    # TODO: Download dummy satellite data
+
+if __name__ == "__main__":
+    setup_environment()
